4c4d10d5d835c0aedd622248484c7c21e7e6ed1a
voxgibson <brandon.stanfield@gmail.com>
2018-09-21 19:41:28 -0400

    Automatic file size adjustment.
    If the final output webm is over 3073000 bytes it will be converted again with a slightly lower bit rate until the the webm's filesize is under 3073000 bytes.

--- webmgeneral.py
+++ webmgeneral.py
@@ -3,7 +3,7 @@ from tkinter import Tk, filedialog, Entry, Label, Button, END
 import os
 import subprocess
 
-# Window Configuraton
+# Window Configuration
 window = Tk()
 window.title('WEBM GENERAL CONVERTER')
 window.resizable(0, 0)
@@ -13,17 +13,19 @@ window['bg'] = '#EEF2FF'
 # Functions
 def inputVideo():
     # Opens input video file dialog.
-    directory = filedialog.askopenfilename(filetypes=[('Video Files', 
-    ' *.3g2 *.3gp *.asf *.avi *.flv *.h264 *.m2t *.m2ts *.m4v *.mkv *.mod *.mov *.mp4 *.mpg *.tod *.vob *.wmv'),
-    ('All', '*.*')])
+    directory = filedialog.askopenfilename(filetypes=[('Video Files',' *.3g2 *.3gp *.asf *.avi *.flv *.m2t *.m2ts *.m4v *.mkv *.mod *.mov *.mp4 *.mpg *.vob *.wmv'),('All', '*.*')])               
+                
+    # Fills in the input video entry field. 
     directoryPath = os.path.normpath(directory)
     inputVideo_ent.delete(0, END)
     inputVideo_ent.insert(0, directoryPath)
 
     # Calculates Maximum Allowed Bitrate.
     inputVideo = str(inputVideo_ent.get())
-    duration = os.popen('ffprobe "' + inputVideo + '" -show_entries format=duration -of compact=p=0:nk=1 -v 0').read()
+    duration = os.popen('ffprobe "' + inputVideo + '" -show_entries format=duration -of compact=p=0:nk=1 -v 0').read()        
     calculation = ((2.985 / float(duration))*8)
+
+    # Fills in 'Calculated Maximum Allowed Bitrate' entry field.
     bitRate = round(calculation, 3)
     bitRate_ent.delete(0, END)
     bitRate_ent.insert(0, bitRate)
@@ -32,17 +34,33 @@ def outputFolder():
     # Opens ouput folder selection dialog. 
     directory = filedialog.askdirectory()
     directoryPath = os.path.normpath(directory)
+
+    # Fills in the output folder entry field. 
     outputFolder_ent.delete(0, END)
     outputFolder_ent.insert(0, directoryPath)
 
 def start():
-    # Starts conversion.
-    inputVideo = str(inputVideo_ent.get())
+    # FFmpeg command variables. 
     bitRate = str(bitRate_ent.get())
-    outputName = os.path.splitext(os.path.basename(inputVideo))[0]
+    inputVideo = str(inputVideo_ent.get())
     outputFolder = str(outputFolder_ent.get())
+    outputName = os.path.splitext(os.path.basename(inputVideo))[0]
     fullPath = str(outputFolder + '\\' + outputName + '.webm')
-    subprocess.Popen('ffmpeg -y -i "' + inputVideo + '" -c:v libvpx -b:v "' + str(bitRate) + '"M -vf scale=1280:-1 -c:a libvorbis -an "' + fullPath)
+
+    # Hides window and starts the conversion.
+    window.withdraw()
+    subprocess.call('ffmpeg -y -i "' + inputVideo + '" -c:v libvpx -b:v "' + str(bitRate) + '"M -vf scale=1280:-1 -c:a libvorbis -an "' + fullPath)
+    
+    # Checks if WebM went over maximum file size. 
+    resultSize = os.path.getsize(fullPath)
+    if resultSize < 3073000:
+        window.deiconify() # <<< Window reappears.
+    else:
+        getRate = float(bitRate_ent.get())-.018 # <<< Subtracts .018 from current bit rate. 
+        newRate = round(getRate,3)
+        bitRate_ent.delete(0, END)
+        bitRate_ent.insert(0, newRate)
+        start() # <<< Starts conversion again at a lower bit rate. 
 
 # Entry Fields
 inputVideo_ent = Entry(fg='#789922', width=45)
